a9e3e2a388c007fd8af36683af94a19bb88a9036
celsoaguiar <caguiar@exchange.slb.com>
2023-04-06 18:27:57 -0700

    cleanup

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -14,19 +14,6 @@ export const APP_ROUTES: Routes = [
       pathMatch: 'full'
     },
 
-    // Your route here:
-
-    // Local usage
-    // {
-    //   path: 'flights',
-    //   loadChildren: () => {
-    //     return loadRemoteModule({
-    //       type: 'module',
-    //       remoteEntry: URL,
-    //       exposedModule: './Module'
-    //     })
-    //     .then(m => m.FlightsModule) }
-    // },
     {
       path: 'flights',
       loadChildren: () => {
@@ -47,8 +34,8 @@ export const APP_ROUTES: Routes = [
         exposedModule: './web-components',
         elementName: 'react-element'
       } as WebComponentWrapperOptions
-    },   
-    
+    },
+
     {
       path: 'angular1',
       component: WebComponentWrapper,
@@ -58,30 +45,8 @@ export const APP_ROUTES: Routes = [
         exposedModule: './web-components',
         elementName: 'angular1-element'
       } as WebComponentWrapperOptions
-    },    
+    },
 
-    {
-      path: 'angular2',
-      component: WebComponentWrapper,
-      data: {
-        remoteEntry: 'https://gray-pond-030798810.azurestaticapps.net//remoteEntry.js',
-        remoteName: 'angular2',
-        exposedModule: './web-components',
-        elementName: 'angular2-element'
-      } as WebComponentWrapperOptions
-    },   
-    
-    {
-      matcher: startsWith('angular3'),
-      component: WebComponentWrapper,
-      data: {
-        remoteEntry: 'https://gray-river-0b8c23a10.azurestaticapps.net/remoteEntry.js',
-        // remoteEntry: 'http://localhost:4202/remoteEntry.js',
-        remoteName: 'angular3',
-        exposedModule: './web-components',
-        elementName: 'angular3-element'
-      } as WebComponentWrapperOptions
-    }, 
     {
       path: 'vue',
       component: WebComponentWrapper,
@@ -91,29 +56,7 @@ export const APP_ROUTES: Routes = [
         exposedModule: './web-components',
         elementName: 'vue-element'
       } as WebComponentWrapperOptions
-    },  
-    
-    {
-      path: 'angularjs',
-      component: WebComponentWrapper,
-      data: {
-        remoteEntry: 'https://calm-mud-0a3ee4a10.azurestaticapps.net/remoteEntry.js',
-        remoteName: 'angularjs',
-        exposedModule: './web-components',
-        elementName: 'angularjs-element'
-      } as WebComponentWrapperOptions
-    },     
-
-    {
-      matcher: startsWith('angular3'),
-      component: WebComponentWrapper,
-      data: {
-        remoteEntry: 'https://gray-river-0b8c23a10.azurestaticapps.net/remoteEntry.js',
-        remoteName: 'angular3',
-        exposedModule: './web-components',
-        elementName: 'angular3-element'
-      } as WebComponentWrapperOptions
-    }, 
+    },
 
     {
       path: 'dashboard',
